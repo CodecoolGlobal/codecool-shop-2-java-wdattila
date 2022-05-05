d3b8cc229ee3b523bc233d506e861fbdd86a513b
wdattila <wdattila@gmail.com>
2022-05-05 12:32:08 +0200

    Add: base shoppingCart model

---
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -0,0 +1,37 @@
+package com.codecool.shop.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ShoppingCart extends BaseModel{
+    private Map<Product, Integer> products;
+
+    public ShoppingCart(String name) {
+        super(name);
+        products = new HashMap<>();
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Map<Product, Integer> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product, int quantity){
+        this.products.put(product, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %1$d, " +
+                        "name: %2$s, " +
+                        "description: %3$s",
+                this.id,
+                this.name,
+                this.description
+        );
+    }
+}
