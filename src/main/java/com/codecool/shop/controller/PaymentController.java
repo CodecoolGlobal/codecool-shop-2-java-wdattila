cb149459a769e38728893817a1c1d982fa13e13c
csipkek98 <csipkekrisztian@gmail.com>
2022-05-05 15:43:24 +0200

    Add: payment route on server for payments

---
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.controller;
+
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PaymentController", urlPatterns = "/payment", loadOnStartup = 3)
+public class PaymentController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        engine.process("paymentPage.html", context, response.getWriter());
+    }
+}
