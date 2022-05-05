d7f90d323663abfdbfbd7f6ffc8b74fffebc49bd
wdattila <wdattila@gmail.com>
2022-05-05 13:14:49 +0200

    Add: create new shoppingCart and fill it

--- src/main/java/com/codecool/shop/controller/PaymentAPIController.java
+++ src/main/java/com/codecool/shop/controller/PaymentAPIController.java
@@ -4,8 +4,11 @@ import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ShoppingCart;
 import com.codecool.shop.service.ProductService;
+import com.codecool.shop.service.ShoppingCartService;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.thymeleaf.TemplateEngine;
@@ -16,6 +19,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -26,19 +30,32 @@ import java.util.stream.Collectors;
 public class PaymentAPIController extends HttpServlet {
     private static final Logger logger
             = (Logger) LoggerFactory.getLogger(ProductController.class);
+    private Gson gson = new Gson();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("Received GET request");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Received POST request");
 
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         Gson gson = new Gson();
 
-        ProductService productService = new ProductService();
-        String productsStr = req.getParameter("products");
+        StringBuilder builder = new StringBuilder();
+        BufferedReader reader = req.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            builder.append(line);
+        }
+        JsonObject products = gson.fromJson(builder.toString(), JsonObject.class);
+
+        ShoppingCartService cartService = new ShoppingCartService();
+
+        ShoppingCart cart = new ShoppingCart("cart");
+        cartService.addShoppingCart(cart);
+        products.keySet().
+                forEach(p -> cartService.addProductToCartById(cart.getId(), Integer.parseInt(p), products.get(p).getAsInt()));
 
         PrintWriter out = resp.getWriter();
-        out.println(gson.toJson(productService.getProductsByIds(productsStr)));
+        out.println(gson.toJson(products));
     }
 }
