fea071164a0c961084597a4729f0fe0dd49ed2cc
wdattila <wdattila@gmail.com>
2022-05-05 14:19:53 +0200

    Add: post api function with json data

--- src/main/webapp/static/js/dataHandler.js
+++ src/main/webapp/static/js/dataHandler.js
@@ -3,12 +3,30 @@ export let dataHandler = {
         let url = new URL("api/products",window.location.href)
         url.searchParams.append("products", productIds);
         return apiGet(url)
+    },
+    get_payment_info : function(data){
+        let url = new URL("api/payment",window.location.href)
+        return apiPost(url, data);
     }
 }
 
 async function apiGet(url){
     const response = await fetch(url,{
         method:"GET"
+    })
+    if(response.status === 200){
+        return await response.json()
+    }
+}
+
+async function apiPost(url, data){
+    const response = await fetch(url,{
+        method:"POST",
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data)
     })
     if(response.status === 200){
         return await response.json()
