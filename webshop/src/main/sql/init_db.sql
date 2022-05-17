14b951556855b4d6e2985fae736a152db256a875
csipkek98 <csipkekrisztian@gmail.com>
2022-05-17 14:09:42 +0200

    Add: default values for init sql

--- webshop/src/main/sql/init_db.sql
+++ webshop/src/main/sql/init_db.sql
@@ -28,7 +28,8 @@ DROP TABLE IF EXISTS Users CASCADE ;
 CREATE TABLE Users(
     id SERIAL NOT NULL PRIMARY KEY,
     name varchar NOT NULL,
-    password varchar NOT NULL
+    password varchar NOT NULL,
+    email varchar NOT NULL
 );
 
 DROP TABLE IF EXISTS Carts CASCADE;
@@ -53,7 +54,7 @@ DROP TABLE IF EXISTS Cart_content;
 CREATE TABLE Cart_content(
     product_id int NOT NULL,
     quantity int NOT NULL,
-    user_id int NOT NULL
+    cart_id int NOT NULL
 );
 
 
@@ -70,4 +71,28 @@ ALTER TABLE ONLY Orders
 ALTER TABLE ONLY Cart_content
     ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES Products(id);
 ALTER TABLE ONLY Cart_content
-    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES Users(id);
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (cart_id) REFERENCES Carts(id);
+
+INSERT INTO Categories(name, department, description) VALUES ('RPG','Video Game', 'Role playing video game type');
+INSERT INTO Categories(name, department, description) VALUES ('Platformer','Video Game', 'Jumping and puzzle solving video game type.');
+INSERT INTO Categories(name, department, description) VALUES ('Action','Video Game', 'Action type video game.');
+INSERT INTO Categories(name, department, description) VALUES ('Strategy','Video Game', 'Strategic type video game.');
+
+INSERT INTO Suppliers(name, description) VALUES ('Nintendont', 'Digital content and services');
+INSERT INTO Suppliers(name, description) VALUES ('Gamma Freak', 'RPG Video game developer');
+INSERT INTO Suppliers(name, description) VALUES ('Camco', 'Video game developer');
+INSERT INTO Suppliers(name, description) VALUES ('Smegma', 'Platformer Video game developer');
+
+INSERT INTO Products(name, price, currency, description, category_id, supplier_id) VALUES ('Chinpokomon: Myrrh', 29.9, 'EUR', 'he next addition of the famous Chinpokomon series. The Myrrh edition contains the legendary chinpokomon Donkeytron.', 1, 2);
+INSERT INTO Products(name, price, currency, description, category_id, supplier_id) VALUES ('Chinpokomon: Platinum', 29.9, 'EUR', 'The next addition of the famous Chinpokomon series. The Platinum edition contains the legendary chinpokomon Lambtron.', 1, 2);
+INSERT INTO Products(name, price, currency, description, category_id, supplier_id) VALUES ('Muckman', 15, 'EUR', 'The Original first addition of the Muckman video game series', 3, 3);
+INSERT INTO Products(name, price, currency, description, category_id, supplier_id) VALUES ('Sanic', 26.9, 'EUR', 'How fast? Sanic fast. Be faster than light with Sanic the hedgehog', 2, 4);
+INSERT INTO Products(name, price, currency, description, category_id, supplier_id) VALUES ('Supra Mayro', 59.99, 'EUR', 'The best seller 90s super classic, Supra Mayro is finally avaliable on Smoke', 2, 1);
+INSERT INTO Products(name, price, currency, description, category_id, supplier_id) VALUES ('Somari', 30, 'EUR', 'The fastest man alive is here. Test your reflexes in a fast paced environment with Somari on Smoke', 2, 1);
+
+INSERT INTO Users(name, password, email) VALUES ('test','test','test@test.com');
+
+INSERT INTO Carts(user_id) VALUES (1);
+
+INSERT INTO Cart_content(product_id, quantity, cart_id) VALUES (1, 2, 1);
+INSERT INTO cart_content(product_id, quantity, cart_id) VALUES (5, 1, 1);
