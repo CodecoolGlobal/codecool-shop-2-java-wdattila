c1e63b0d9e5c75b105a80349e97d6f3fde7bc017
wdattila <wdattila@gmail.com>
2022-05-17 11:44:00 +0200

    Add: ProductCategoryDaoJdbc basic Dao implementation

--- webshop/src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -1,4 +1,33 @@
 package com.codecool.shop.dao.implementation;
 
-public class ProductCategoryDaoJdbc {
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import java.util.List;
+
+public class ProductCategoryDaoJdbc implements ProductCategoryDao {
+    @Override
+    public void add(ProductCategory category) {
+
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<ProductCategory> getMultipleById(String ids) {
+        return null;
+    }
 }
