7b9de755e8aeb1b62d7ac6a7f5878c24b37eb0f6
wdattila <wdattila@gmail.com>
2022-05-17 11:34:55 +0200

    Add: ShoppingCartDaoJdbc basic dao implementation

--- webshop/src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/ShoppingCartDaoJdbc.java
@@ -1,4 +1,33 @@
 package com.codecool.shop.dao.implementation;
 
-public class ShoppingCartDaoJdbc {
+import com.codecool.shop.dao.ShoppingCartDao;
+import com.codecool.shop.model.ShoppingCart;
+
+import java.util.List;
+
+public class ShoppingCartDaoJdbc implements ShoppingCartDao {
+    @Override
+    public void add(ShoppingCart shoppingCart) {
+
+    }
+
+    @Override
+    public ShoppingCart find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ShoppingCart> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<ShoppingCart> getMultipleById(String ids) {
+        return null;
+    }
 }
