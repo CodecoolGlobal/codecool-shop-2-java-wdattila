00713e7b9f48cadc248304bff296c6989f05a60c
wdattila <wdattila@gmail.com>
2022-05-17 11:45:45 +0200

    Add: ProductDaoJdbc basic Dao implementation

--- webshop/src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -1,4 +1,60 @@
 package com.codecool.shop.dao.implementation;
 
-public class ProductDaoJdbc {
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+public class ProductDaoJdbc implements ProductDao {
+    @Override
+    public void add(Product product) {
+
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getMultipleByCategories(List<ProductCategory> productCategories) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getMultipleBySuppliers(List<Supplier> suppliers) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getMultipleByCategoriesSuppliers(List<ProductCategory> productCategories, List<Supplier> suppliers) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getMultipleById(String ids) {
+        return null;
+    }
 }
