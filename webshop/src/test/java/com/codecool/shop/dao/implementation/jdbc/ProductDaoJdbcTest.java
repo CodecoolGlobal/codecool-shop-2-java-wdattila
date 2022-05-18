9eefa041c59b9ef91d4f935f3b579835a1a93315
wdattila <wdattila@gmail.com>
2022-05-18 13:47:00 +0200

    Add: test class for productDaoMem

---
+++ webshop/src/test/java/com/codecool/shop/dao/implementation/jdbc/ProductDaoJdbcTest.java
@@ -0,0 +1,58 @@
+package com.codecool.shop.dao.implementation.jdbc;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoJdbcTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void testGetBy() {
+    }
+
+    @Test
+    void getMultipleByCategories() {
+    }
+
+    @Test
+    void getMultipleBySuppliers() {
+    }
+
+    @Test
+    void getMultipleByCategoriesSuppliers() {
+    }
+
+    @Test
+    void getMultipleById() {
+    }
+}
