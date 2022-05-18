0fd90f1615392352ce65018fd97503a6dde4ec07
wdattila <wdattila@gmail.com>
2022-05-18 11:16:13 +0200

    Add: test class for supplierdaojdbc

---
+++ webshop/src/test/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbcTest.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.dao.implementation.jdbc;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SupplierDaoJdbcTest {
+
+    @BeforeAll
+    static void setUp() {
+    }
+
+    @AfterAll
+    static void tearDown() {
+    }
+
+    @Test
+    @Order(1)
+    void add() {
+    }
+
+    @Test
+    @Order(2)
+    void find() {
+    }
+
+    @Test
+    @Order(3)
+    void remove() {
+    }
+
+    @Test
+    @Order(4)
+    void getAll() {
+    }
+
+    @Test
+    @Order(5)
+    void getMultipleById() {
+    }
+}
