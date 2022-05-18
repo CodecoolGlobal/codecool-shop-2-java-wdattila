e5f57f8b005e4564d294e8d8518c45cd4cc054cc
wdattila <wdattila@gmail.com>
2022-05-18 14:55:40 +0200

    Add: find method in ProductDaoJdbc

--- webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/ProductDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/ProductDaoJdbc.java
@@ -1,18 +1,27 @@
 package com.codecool.shop.dao.implementation.jdbc;
 
+import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.*;
 import java.util.List;
 
 public class ProductDaoJdbc implements ProductDao {
     private DataSource dataSource;
+    private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
 
-    public ProductDaoJdbc(DataSource dataSource) {
+    public ProductDaoJdbc(DataSource dataSource, ProductCategoryDao productCategoryDao, SupplierDao supplierDao) {
         this.dataSource = dataSource;
+        this.productCategoryDao = productCategoryDao;
+        this.supplierDao = supplierDao;
     }
 
     @Override
@@ -22,7 +31,33 @@ public class ProductDaoJdbc implements ProductDao {
 
     @Override
     public Product find(int id) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT name, price, currency, description, category_id, supplier_id FROM Products " +
+                    "WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            if(rs.next()){
+                String name = rs.getString("name");
+                BigDecimal price = rs.getBigDecimal("price");
+                String currency = rs.getString("currency");
+                String description = rs.getString("description");
+                int categoryId = rs.getInt("category_id");
+                int supplierId = rs.getInt("supplier_id");
+                Product product = new Product(name,
+                        price.setScale(2, RoundingMode.HALF_UP),
+                        currency,
+                        description,
+                        productCategoryDao.find(categoryId),
+                        supplierDao.find(supplierId));
+                product.setId(id);
+                return product;
+            }else{
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
