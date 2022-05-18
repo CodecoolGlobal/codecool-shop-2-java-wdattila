759a125c45e5e45ef027da48042fd0effa200217
wdattila <wdattila@gmail.com>
2022-05-18 11:22:01 +0200

    Add: supplierdaojdbc test setup

--- webshop/src/test/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbcTest.java
+++ webshop/src/test/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbcTest.java
@@ -1,16 +1,54 @@
 package com.codecool.shop.dao.implementation.jdbc;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.SupplierDao;
 import org.junit.jupiter.api.*;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SupplierDaoJdbcTest {
 
+    private static SupplierDao supplierDao;
+    private static void initDatabase() throws SQLException {
+        DataSource dataSource = JdbcTestUtil.getSource();
+        try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE Suppliers(" +
+                    "    id SERIAL NOT NULL PRIMARY KEY," +
+                    "    name varchar NOT NULL," +
+                    "    description varchar" +
+                    ")");
+            connection.commit();
+            statement.executeUpdate("INSERT INTO Suppliers(name, description) VALUES ('Gamma Freak', 'RPG Video game developer')");
+            statement.executeUpdate("INSERT INTO Suppliers(name, description) VALUES ('Nintendont', 'Digital content and services');");
+            connection.commit();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @BeforeAll
-    static void setUp() {
+    public static void init() throws SQLException, ClassNotFoundException, IOException {
+        Class.forName("org.hsqldb.jdbc.JDBCDriver");
+
+        // initialize database
+        initDatabase();
+        supplierDao = new SupplierDaoJdbc(JdbcTestUtil.getSource());
     }
 
     @AfterAll
-    static void tearDown() {
+    public static void destroy() throws SQLException, ClassNotFoundException, IOException {
+        DataSource dataSource = JdbcTestUtil.getSource();
+        try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement();) {
+            statement.executeUpdate("DROP TABLE Suppliers");
+            connection.commit();
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
