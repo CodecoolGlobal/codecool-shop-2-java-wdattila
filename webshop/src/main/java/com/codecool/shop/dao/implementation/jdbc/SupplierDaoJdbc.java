29b96e5053150e44d42ad97bed532f7acfd6dc17
wdattila <wdattila@gmail.com>
2022-05-18 12:13:47 +0200

    Add: add method in SupplierDaoJdbc

--- webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
@@ -17,7 +17,19 @@ public class SupplierDaoJdbc implements SupplierDao {
 
     @Override
     public void add(Supplier supplier) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO Suppliers (name, description) " +
+                    "VALUES (?, ?)";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, supplier.getName());
+            statement.setString(2, supplier.getDescription());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            resultSet.next();
+            supplier.setId(resultSet.getInt(1));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
