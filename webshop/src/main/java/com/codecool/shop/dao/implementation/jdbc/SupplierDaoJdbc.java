40f040f3ef42800081328cf90bd4f893d9179fe5
wdattila <wdattila@gmail.com>
2022-05-18 12:15:47 +0200

    Add: remove method in SupplierDaoJdbc

--- webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/SupplierDaoJdbc.java
@@ -56,7 +56,15 @@ public class SupplierDaoJdbc implements SupplierDao {
 
     @Override
     public void remove(int id) {
-
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "DELETE FROM Suppliers " +
+                    "WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
