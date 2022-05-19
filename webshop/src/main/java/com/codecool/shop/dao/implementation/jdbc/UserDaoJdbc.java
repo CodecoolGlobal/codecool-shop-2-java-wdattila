8263b79e747b9c0bf8c247e7a6eb3274e999bdcf
csipkek98 <csipkekrisztian@gmail.com>
2022-05-19 11:47:07 +0200

    Add: method to find users by id, or with username and email

--- webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/UserDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/UserDaoJdbc.java
@@ -38,6 +38,47 @@ public class UserDaoJdbc implements UserDao {
 
     @Override
     public User find(int id) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT name, password, email, salt FROM Users " +
+                    "WHERE id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                String name = rs.getString("name");
+                byte[] password = HashManager.stringMatrixToHash(rs.getString("password"));
+                String email = rs.getString("email");
+                byte[] salt = HashManager.stringMatrixToHash(rs.getString("salt"));
+                User user = new User(name, email, password, salt);
+                user.setId(id);
+                return user;
+            }
+        } catch (SQLException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public User find(String name, String email) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, password, email, salt FROM Users " +
+                    "WHERE name = ? OR email = ?";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, name);
+            statement.setString(2, email);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                int id = rs.getInt("id");
+                byte[] password = HashManager.stringMatrixToHash(rs.getString("password"));
+                byte[] salt = HashManager.stringMatrixToHash(rs.getString("salt"));
+                User user = new User(name, email, password, salt);
+                user.setId(id);
+                return user;
+            }
+        } catch (SQLException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
