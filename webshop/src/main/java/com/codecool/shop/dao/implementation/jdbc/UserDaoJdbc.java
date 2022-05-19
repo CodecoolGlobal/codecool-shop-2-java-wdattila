6af351d68e3c5ead74d207c5696c5ffe6d0195a0
csipkek98 <csipkekrisztian@gmail.com>
2022-05-19 11:40:53 +0200

    Add: UserDaoJdbc constructor with dataSource for sql operations

--- webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/UserDaoJdbc.java
+++ webshop/src/main/java/com/codecool/shop/dao/implementation/jdbc/UserDaoJdbc.java
@@ -1,10 +1,21 @@
 package com.codecool.shop.dao.implementation.jdbc;
 
 import com.codecool.shop.dao.UserDao;
-import com.codecool.shop.model.Supplier;
 import com.codecool.shop.model.User;
+import com.codecool.shop.service.HashManager;
+
+import javax.sql.DataSource;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.*;
 
 public class UserDaoJdbc implements UserDao {
+
+    private DataSource dataSource;
+
+    public UserDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Override
     public void add(User user) {
